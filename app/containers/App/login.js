ea28f7f2cf0ec6ae60a1386bb3d1ec3b2ea574f4
dwj <dwi@163.com>
2017-09-25 22:36:18 +0800

    登录页面重写

---
+++ app/containers/App/login.js
@@ -0,0 +1,18 @@
+/**
+ *  Created by daiwenjuan on 17/9/21 下午5:38.
+ */
+import './login.less'
+import React, { PureComponent } from 'react'
+import { Spin, message, Form, Icon, Input, Button, Row, Col } from 'antd'
+
+export default class Login extends PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return ( <div className='login'>
+
+    </div>)
+  }
+}
